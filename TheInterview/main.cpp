ba4ed7ee0e5926532ce6ee80f9ac0d372e5721e8
CemilKes <kes.cemil@gmail.com>
2020-12-28 10:43:02 -0800

    Commit changes

---
+++ TheInterview/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TheInterview
+//
+//  Created by Mr Kes on 12/28/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
